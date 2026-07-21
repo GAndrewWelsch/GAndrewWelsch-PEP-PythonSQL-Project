29db562123c621a68d2f0e7208760b57b164c662
GAndrewWelsch <GAndrewWelsch@github.com>
2026-07-21 19:45:35 +0000

    Test run: 0/5 passed, 5 failed, 0 skipped - 2026-07-21T19:45:35.633Z

--- src/main/main.py
+++ src/main/main.py
@@ -52,7 +52,7 @@ def load_and_clean_users(file_path):
     with open(file_path) as userFile:
         userReader = csv.reader(userFile, delimiter=' ', quotechar='|')
         for row in userReader:
-            cursor.execute("INSERT INTO users (firstName, lastName) VALUES ({row[0], row[1]})")
+            cursor.execute(f"INSERT INTO users (firstName, lastName) VALUES ({row[0]}, {row[1]})")
     print("TODO: load_users")
 
 
