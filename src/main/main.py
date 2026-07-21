d280b80e2aca01e346fef6db605a3329a9d868ef
GAndrewWelsch <GAndrewWelsch@github.com>
2026-07-21 20:21:03 +0000

    Test run: 0/4 passed, 0 failed, 0 skipped - 2026-07-21T20:21:03.309Z

--- src/main/main.py
+++ src/main/main.py
@@ -53,7 +53,7 @@ def load_and_clean_users(file_path):
         userReader = csv.reader(userFile)
         header = next(userFile)
         for row in userReader:
-            alphalist = list(map(str.isalpha(), row))
+            alphalist = list(map(str.isalpha, row))
             if (len(row) == 2 and all(alphalist)):
                 cursor.execute(("INSERT INTO users(firstName, lastName) VALUES (?, ?)"), (row[0], row[1]))
             conn.commit()
