e19374ecec27710b63271d586aec270321b483a2
GAndrewWelsch <GAndrewWelsch@github.com>
2026-07-21 20:25:24 +0000

    Test run: 0/5 passed, 5 failed, 0 skipped - 2026-07-21T20:25:24.516Z

--- src/main/main.py
+++ src/main/main.py
@@ -53,7 +53,7 @@ def load_and_clean_users(file_path):
         userReader = csv.reader(userFile)
         header = next(userFile)
         for row in userReader:
-            if (len(row) == 2 and row[0] != " " and row[1] != " "):
+            if (len(row) == 2 and row[0] and row[1]):
                 cursor.execute(("INSERT INTO users(firstName, lastName) VALUES (?, ?)"), (row[0], row[1]))
             conn.commit()
 
