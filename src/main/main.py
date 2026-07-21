dbf43e0b3244229f8c3b182ac94d9b92cb4ed328
GAndrewWelsch <GAndrewWelsch@github.com>
2026-07-21 20:24:33 +0000

    Test run: 0/5 passed, 5 failed, 0 skipped - 2026-07-21T20:24:33.533Z

--- src/main/main.py
+++ src/main/main.py
@@ -53,7 +53,7 @@ def load_and_clean_users(file_path):
         userReader = csv.reader(userFile)
         header = next(userFile)
         for row in userReader:
-            if (len(row) == 2 and row[0].isalpha() and row[1].isalpha()):
+            if (len(row) == 2 and row[0] != " " and row[1] != " "):
                 cursor.execute(("INSERT INTO users(firstName, lastName) VALUES (?, ?)"), (row[0], row[1]))
             conn.commit()
 
@@ -62,9 +62,9 @@ def load_and_clean_users(file_path):
 def load_and_clean_call_logs(file_path):
     with open(file_path) as logFile:
         logReader = csv.reader(logFile)
-        header = next(userFile)
+        header = next(logFile)
         for row in logReader:
-            if (len(row) == 2 and row[0].isalpha() and row[1].isalpha()):
+            if (len(row) == 5 and row[0].isalpha() and row[1].isalpha()):
                 cursor.execute(("INSERT INTO users(firstName, lastName) VALUES (?, ?)"), (row[0], row[1]))
             conn.commit()
 
