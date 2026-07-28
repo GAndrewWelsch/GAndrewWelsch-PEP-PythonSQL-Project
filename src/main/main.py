75696e6e41dfe3c39549e3b58639e61975a68690
GAndrewWelsch <GAndrewWelsch@github.com>
2026-07-28 15:32:17 +0000

    Test run: 2/4 passed, 2 failed, 0 skipped - 2026-07-28T15:32:17.757Z

--- src/main/main.py
+++ src/main/main.py
@@ -30,7 +30,7 @@ def main():
     )''')
 
     # You will implement these methods below. They just print TO-DO messages for now.
-    load_and_clean_users('../../resources/users.csv')
+    load_and_clean_users('../resources/users.csv')
     load_and_clean_call_logs('../../resources/callLogs.csv')
     write_user_analytics('../../resources/userAnalytics.csv')
     write_ordered_calls('../../resources/orderedCalls.csv')
