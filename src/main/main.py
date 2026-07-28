90960f2c83bdf056ba929dd87329108c6f48cac9
GAndrewWelsch <GAndrewWelsch@github.com>
2026-07-28 16:33:24 +0000

    Test run: 4/4 passed, 0 failed, 0 skipped - 2026-07-28T16:33:24.136Z

--- src/main/main.py
+++ src/main/main.py
@@ -30,10 +30,10 @@ def main():
     )''')
 
     # You will implement these methods below. They just print TO-DO messages for now.
-    load_and_clean_users('resources/users.csv')
-    load_and_clean_call_logs('resources/callLogs.csv')
-    write_user_analytics('resources/userAnalytics.csv')
-    write_ordered_calls('resources/orderedCalls.csv')
+    load_and_clean_users('../../resources/users.csv')
+    load_and_clean_call_logs('../../resources/callLogs.csv')
+    write_user_analytics('../../resources/userAnalytics.csv')
+    write_ordered_calls('../../resources/orderedCalls.csv')
 
     # Helper method that prints the contents of the users and callLogs tables. Uncomment to see data.
     # select_from_users_and_call_logs()
